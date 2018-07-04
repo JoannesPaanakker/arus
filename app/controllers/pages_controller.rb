03b30c0b032af27e6e897547513e5f611e9cd61d
Joannes Paanakker <j.paanakker@upcmail.nl>
2018-07-04 16:56:05 +0200

    add grid

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,20 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :appartment, :boat, :bookings]
 
   def home
+    @page = "home"
   end
+
+  def appartment
+    @page = "appartment"
+  end
+
+  def boat
+    @page = "boat"
+  end
+
+  def bookings
+    @page = "bookings"
+  end
+
 end
